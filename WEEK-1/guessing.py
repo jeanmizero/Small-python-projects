daf5a517ccd6092a7658c5c07324d38d92aeebbf
Jean <mizero2@gmail.com>
2021-05-21 22:31:57 -0600

    add Black jack game

--- WEEK-1/guessing.py
+++ WEEK-1/guessing.py
@@ -9,3 +9,13 @@ while True:
         print("TOO HIGH")
     else:
         print("YOU WIN!")
+# Black Jack game
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+    user_cards = []
+    computer_cards = []
